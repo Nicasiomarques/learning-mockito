7e911d671f1221538a3ec0a5b00183508eabe278
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-10 12:09:58 +0100

    feat: add constructor, method proximoDiaUtil, clock to prevent static methods on tests

--- src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
+++ src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
@@ -1,6 +1,9 @@
 package br.com.alura.leilao.service;
 
+import java.time.Clock;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,14 +14,25 @@ import br.com.alura.leilao.model.Pagamento;
 
 @Service
 public class GeradorDePagamento {
+	private PagamentoDao pagamentos;
+	private Clock clock;
 
 	@Autowired
-	private PagamentoDao pagamentos;
+	public GeradorDePagamento(PagamentoDao pagamentos, Clock clock) {
+		this.pagamentos = pagamentos;
+		this.clock = clock;
+	}
 
 	public void gerarPagamento(Lance lanceVencedor) {
-		LocalDate vencimento = LocalDate.now().plusDays(1);
-		Pagamento pagamento = new Pagamento(lanceVencedor, vencimento);
+		LocalDate vencimento = LocalDate.now(clock).plusDays(1);
+		Pagamento pagamento = new Pagamento(lanceVencedor, proximoDiaUtil(vencimento));
 		this.pagamentos.salvar(pagamento);
 	}
 
+	private LocalDate proximoDiaUtil(LocalDate data) {
+		int diasAAdicionar = Map
+		.of(DayOfWeek.SATURDAY, 2, DayOfWeek.SUNDAY, 1)
+		.getOrDefault(data.getDayOfWeek(), 0);
+		return data.plusDays(diasAAdicionar);
+	}
 }
